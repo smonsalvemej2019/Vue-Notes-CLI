a8634bf2e2486fd25309b56c72521d8b8fe104c1
Santiago Monsalve <71195627+smonsalvemej2019@users.noreply.github.com>
2021-05-11 17:14:34 -0400

    first commit

---
+++ vue-client-notes/src/App.vue
@@ -0,0 +1,66 @@
+<template>
+  <div class="container">
+    <!--we have a component called Header-->
+    <!--we can pass props into our components-->
+    <!--ex we can pass a title with the value Task Tracker-->
+  <Header title="Task Tracker"/>
+  </div>
+</template>
+
+<script>
+//first we import our components...
+import Header from './components/Header'
+
+export default {
+  name: 'App',
+  components: {
+    //...then register them in the export
+    Header,
+  }
+}
+</script>
+
+<style>
+/*this style is global*/
+@import url('https://fonts.googleapis.com/css2?family=Poppins:wght@300;400&display=swap');
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+}
+body {
+  font-family: 'Poppins', sans-serif;
+}
+.container {
+  max-width: 500px;
+  margin: 30px auto;
+  overflow: auto;
+  min-height: 300px;
+  border: 1px solid steelblue;
+  padding: 30px;
+  border-radius: 5px;
+}
+.btn {
+  display: inline-block;
+  background: #000;
+  color: #fff;
+  border: none;
+  padding: 10px 20px;
+  margin: 5px;
+  border-radius: 5px;
+  cursor: pointer;
+  text-decoration: none;
+  font-size: 15px;
+  font-family: inherit;
+}
+.btn:focus {
+  outline: none;
+}
+.btn:active {
+  transform: scale(0.98);
+}
+.btn-block {
+  display: block;
+  width: 100%;
+}
+</style>
---
+++ vue-client-notes/src/components/Header.vue
@@ -0,0 +1,31 @@
+<!--the html for this component-->
+<template>
+    <header>
+        <h1>{{ title }}</h1>
+    </header>
+</template>
+<!--we are exporting this template so it can be mounted by the app.vue-->
+<script>
+export default {
+    name: 'Header',
+    props: {
+        //from the app.vu we are passing a prop called title
+        //the title is a type string with a default value
+        //if we get rid of the prop in app.js, it will display default
+        title:{
+            type: String,
+            default: 'Hello World'
+        }
+    },
+}
+</script>
+<!--style is going to be scoped for this component only-->
+<style scoped>
+    header {
+        display: flex;
+        justify-content: space-between;
+        align-items: center;
+        margin-bottom: 20px;
+    }
+</style>
+
